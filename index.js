c51187408529be0719bd916681956e3cdec6e2da
keaveh <kaveh.zohdi96@gmail.com>
2020-12-15 21:21:53 +0330

    add file

---
+++ index.js
@@ -0,0 +1,12 @@
+//{"id":"kaveh_zohdi", "following":"20", "followers":"20"}var request = new XMLHttpRequest();
+// request.open('GET', 'test.txt' );
+// request.send();
+
+// if( ( request.status === 200 ) ){
+//    console.log(request);
+// }
+
+function functionName() {
+   console.log("Hello World");
+ }
+ 
